3571307c1bcae2d033136d5d5580d90933b6b593
Umesh <umeshtamang129@gmail.com>
2025-01-28 15:32:32 +0545

    try-except

--- apiproject/api/views.py
+++ apiproject/api/views.py
@@ -3,8 +3,11 @@ from rest_framework.response import Response
 from rest_framework.decorators import APIView
 from rest_framework import status
 from rest_framework.authentication import BasicAuthentication,TokenAuthentication
+import logging
 
 # Create your views here.
+
+logger=logging.getLogger('django')
 
 class FirstApiTestView(APIView):
         
@@ -12,10 +15,17 @@ class FirstApiTestView(APIView):
         permission_class=[]
 
         def get(self,request):
+            try:
                 msg={
                         "response":"success",
                         "data":[{"name":"Umesh","address":"Bhaktapur"}]
                 }
                 return Response(msg,status=status.HTTP_200_OK)
+            except Exception as e:
+                 logger.info(str(e),exc_info=True)
+                 msg={
+                      "response":"fail"
+                 }
+                 return Response(msg,status=status.HTTP_400_BAD_REQUEST)
         
 
