23828dd8dc5478963deebf5d9269549532082214
Umesh <umeshtamang129@gmail.com>
2025-01-28 15:19:19 +0545

    generate auth token

--- apiproject/api/views.py
+++ apiproject/api/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import APIView
+from rest_framework import status
+from rest_framework.authentication import BasicAuthentication,TokenAuthentication
 
 # Create your views here.
+
