37161fdc5cd946ba9b792d3200cbcbb246562976
Umesh <umeshtamang129@gmail.com>
2025-01-28 15:11:38 +0545

    create a student model and customize admin

--- apiproject/api/admin.py
+++ apiproject/api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Student
 
 # Register your models here.
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display=["id","name","address","age","mobile_number"]
+    list_display_links=["name"]
+
+admin.site.register(Student,StudentAdmin)
--- apiproject/api/models.py
+++ apiproject/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    name=models.CharField(max_length=100)
+    address=models.CharField(max_length=100)
+    age=models.IntegerField()
+    mobile_number=models.CharField(max_length=12)
+
+    def __str__(self):
+        return self.name
