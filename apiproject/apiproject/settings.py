5eedcdfbb1032d3435da18246afc46f2df80608c
Umesh <umeshtamang129@gmail.com>
2025-01-28 14:50:56 +0545

    connect mysql database

--- apiproject/apiproject/settings.py
+++ apiproject/apiproject/settings.py
@@ -66,10 +66,15 @@ WSGI_APPLICATION = 'apiproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# mysql
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'rest_api',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
